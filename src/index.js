f82becad923ba37f9f8db5232a2c8f2c13ee5cc6
Olga Sharapo <olgasharapo2@gmail.com>
2021-03-15 23:43:30 +0300

    add function components, hook-useState

--- src/index.js
+++ src/index.js
@@ -152,13 +152,58 @@ const reconcileChildren = (wipFiber, elements) => {
   }
 }
 
-const performUnitOfWork = (fiber) => {
+let wipFiber = null;
+let hookIndex = null;
+
+const updateFunctionComponent = (fiber) => {
+  wipFiber = fiber;
+  hookIndex = 0;
+  wipFiber.hooks = [] 
+  const children = [fiber.type(fiber.props)];
+  reconcileChildren(fiber, children)
+};
+
+const updateHostComponent = (fiber) => {
   if (!fiber.dom) {
     fiber.dom = createDom(fiber);
   }
+  const { children } = fiber.props;
+  reconcileChildren(fiber, children);
+}
 
-  const elements = fiber.props.children;
-  reconcileChildren(fiber, elements);
+const useState = (initial) => {
+  const oldHook = wipFiber.alternate && wipFiber.alternate.hooks && wipFiber.alternate.hooks[hookIndex];
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: [],
+  }
+
+  const actions = oldHook ? oldHook.queue : [];
+  actions.forEach(action => hook.state = action(hook.state));
+  
+  const setState = (action) => {
+    hook.queue.push(action);
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot,
+    }
+    nextUnitOfWork = wipRoot;
+    deletions = [];
+  }
+
+  wipFiber.hooks.push(hook);
+  hookIndex++
+  return [hook.state, setState];
+}
+
+const performUnitOfWork = (fiber) => {
+  const isFunctionComponent = fiber.type instanceof Function;
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber);
+  } else {
+    updateHostComponent(fiber);
+  }
 
   if (fiber.child) {
     return fiber.child
@@ -172,12 +217,25 @@ const performUnitOfWork = (fiber) => {
   }
 }
 
+const commitDeletion = (fiber, domParent) => {
+  if (fiber.dom) {
+    domParent.removeChild(fiber.dom);
+  } else {
+    commitDeletion(fiber.child, domParent);
+  }
+}
+
 const commitWork = (fiber) => {
   if (!fiber) {
     return
   }
 
-  const domParent = fiber.parent.dom
+  let domParentFiber = fiber.parent;
+  while (!domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent;
+  }
+  const domParent = domParentFiber.dom;
+
   if (
     fiber.effectTag === "PLACEMENT" &&
     fiber.dom != null
@@ -193,7 +251,7 @@ const commitWork = (fiber) => {
       fiber.props
     )
   } else if (fiber.effectTag === "DELETION") {
-    domParent.removeChild(fiber.dom)
+    commitDeletion(fiber, domParent);
   }
 
   commitWork(fiber.child)
@@ -226,6 +284,7 @@ window.requestIdleCallback(workLoop);
 const Didact = {
   createElement,
   render,
+  useState,
 }
 
 /** @jsxRuntime classic */
@@ -282,22 +341,59 @@ const Didact = {
 // const container = document.getElementById("root")
 // Didact.render(element, container);
 
-const container = document.getElementById("root")
 
-const updateValue = e => {
-  console.log(e.target.value);
-  rerender(e.target.value)
-}
+// // example
+// const container = document.getElementById("root")
 
-const rerender = value => {
-  console.log(value);
-  const element = (
+// const updateValue = e => {
+//   console.log(e.target.value);
+//   rerender(e.target.value)
+// }
+
+// const rerender = value => {
+//   console.log(value);
+//   const element = (
+//     <div>
+//       <input onInput={updateValue} value={value} />
+//       <h2>Hello {value}</h2>
+//     </div>
+//   )
+//   Didact.render(element, container)
+// }
+
+// rerender("World")
+
+
+// // example
+// const App = (props) => <h1>Hi {props.name}</h1>
+// // const App = (props) => (
+// //   Didact.createElement('h1', null, "Hi", props.name);
+// // )
+
+// const element = <App name="foo" />
+// // const element = Didact.createElement(App, { name: "foo" });
+
+// const container = document.getElementById('root');
+
+// Didact.render(element, container);
+
+// example
+const Counter = () => {
+  const [value, setValue] = Didact.useState(1);
+  
+  return (
     <div>
-      <input onInput={updateValue} value={value} />
-      <h2>Hello {value}</h2>
+      <h1>
+        Count: {value}
+      </h1>
+      <p>
+        <button onClick={() => setValue((value) => value + 1)}>+</button>
+      </p>
     </div>
   )
-  Didact.render(element, container)
 }
 
-rerender("World")
+const element = <Counter />
+const container = document.getElementById('root');
+
+Didact.render(element, container);
